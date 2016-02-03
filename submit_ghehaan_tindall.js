2fe9392f09ff60941f8d38f4d137a4f385a15d8b
Ghehaan <ghehaan@yahoo.com>
2016-02-03 16:12:19 +0930

    Mobile View: Added open close functionality

--- submit_ghehaan_tindall.js
+++ submit_ghehaan_tindall.js
@@ -1,1 +1,27 @@
 // JavaScript Document
+
+function toggleItem(itemHeaderId,itemBodyId)
+{
+	var itemHeaderClassName = document.getElementById(itemHeaderId).className;
+	var itemBodyClassName = document.getElementById(itemBodyId).className;
+	
+	if(itemHeaderClassName.indexOf("expanded")>=0)
+	{
+		itemHeaderClassName = itemHeaderClassName.replace("expanded", "contracted");
+	}
+	else
+	{
+		itemHeaderClassName = itemHeaderClassName.replace("contracted", "expanded");
+	}
+	document.getElementById(itemHeaderId).className = itemHeaderClassName;
+	
+	if(itemBodyClassName.indexOf("open")>=0)
+	{
+		itemBodyClassName = itemBodyClassName.replace("open", "closed");
+	}
+	else
+	{
+		itemBodyClassName = itemBodyClassName.replace("closed", "open");
+	}
+	document.getElementById(itemBodyId).className = itemBodyClassName;
+}
