3ef051f32771cc8c4fd2e67b5121d2208f05b766
Ghehaan <ghehaan@yahoo.com>
2016-02-04 17:44:33 +0930

    Tabbed features added

--- submit_ghehaan_tindall.js
+++ submit_ghehaan_tindall.js
@@ -3,10 +3,37 @@
 function toggleItem(itemHeaderId,itemBodyId)
 {
 	toggleAccordian(itemHeaderId,itemBodyId);	
-	
+	toggleTab(itemHeaderId,itemBodyId);		
 }
 
-
+function toggleTab(itemHeaderId,itemBodyId)
+{
+	var itemHeaderClassName = document.getElementById(itemHeaderId).className;
+	var itemBodyClassName = document.getElementById(itemBodyId).className;
+	
+	var children = document.getElementById(itemHeaderId).parentNode.parentNode.children;
+	if(itemHeaderClassName.indexOf("currentTab")<0)
+	{
+		itemHeaderClassName = itemHeaderClassName + " currentTab";		
+		itemBodyClassName = itemBodyClassName + " openTab";				
+		for (i = 0; i < children.length; i++) { 
+			if(children[i].children.length>0)
+			{
+				var itemClass = children[i].children[0].className;
+				itemClass = itemClass.replace("currentTab", "");
+				var itemId = children[i].children[0].id;
+				document.getElementById(itemId).className = itemClass;
+				
+				var bodyClass = children[i].children[1].className;
+				bodyClass = bodyClass.replace("openTab", "");
+				var bodyId = children[i].children[1].id;
+				document.getElementById(bodyId).className = bodyClass;				
+			}
+		}
+	}
+	document.getElementById(itemHeaderId).className = itemHeaderClassName;
+	document.getElementById(itemBodyId).className = itemBodyClassName;
+}
 
 function toggleAccordian(itemHeaderId,itemBodyId)
 {
